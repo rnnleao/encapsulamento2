466b194baf6c056b606652f8d528b72cfcdaa352
rnnleao <renancleao@outlook.com>
2023-04-11 13:38:22 +0000

    Pessoa java

---
+++ Pessoa.java
@@ -0,0 +1,15 @@
+public class Pessoa {
+
+    private String nome;
+    private int idade;
+    private String celular;
+    private String cpf;
+    
+
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+}
